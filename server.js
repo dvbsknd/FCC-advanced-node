e4ad5aa32c88a761748b2a89dd4f750a4b849cd4
Dave Baskind <dave@picketstudio.com>
2020-08-31 08:29:38 +1000

    Configures pug to serve index

--- server.js
+++ server.js
@@ -9,10 +9,11 @@ fccTesting(app); //For FCC testing purposes
 app.use("/public", express.static(process.cwd() + "/public"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.set('views', './views/pug'); // Will actually default here anyway
+app.set('view engine', 'pug'); // Import/require not required
 
 app.route("/").get((req, res) => {
-  //Change the response to render the Pug template
-  res.send(`Pug template is not defined.`);
+  res.render('index');
 });
 
 app.listen(process.env.PORT || 3000, () => {
