a174dbe014da30a2c3db286619a8479ae84d61a3
Dave Baskind <dave@picketstudio.com>
2020-09-07 09:10:57 +1000

    Implement registration

--- server.js
+++ server.js
@@ -83,6 +83,30 @@ passport.use(new LocalStrategy((username, password, done) => {
   });
 }));
 
+const registerUser = (req, res, next) => {
+  const client = new MongoClient(process.env.MONGO_URI, { useUnifiedTopology: true });
+  client.connect(err => {
+    if (err) return console.error(err);
+    const db = client.db();
+    db.collection('users').findOne(
+      { username: req.body.username },
+      (err, user) => {
+        if (err) { next(err) }
+        else if (user) { res.redirect('/'); }
+        else {
+          db.collection('users').insertOne(
+            { username: req.body.username, password: req.body.password },
+            (err, doc) => {
+              console.log('Adding user \x1b[33m%s\x1b[0m', req.body.username);
+              if (err) { res.redirect('/'); }
+              else { next(null, user); }
+            }
+          );
+        };
+      });
+  });
+}
+
 const ensureAuthenticated = (req, res, next) => {
   if (req.isAuthenticated()) {
     console.log('\x1b[33m%s\x1b[0m logged in, serving request', req.user._id);
@@ -100,7 +124,8 @@ app.route("/").get((req, res) => {
   const data = {
     title: 'Home Page',
     message: 'Please login',
-    showLogin: true
+    showLogin: true,
+    showRegistration: true
   };
   res.render('index', data);
 });
@@ -112,7 +137,16 @@ app.route('/profile').get(ensureAuthenticated, (req, res) => {
   res.render('profile', data);
 });
 
-app.post('/login', passport.authenticate('local', { failureRedirect: '/' }), (req, res) => { 
+app.post('/register',
+  registerUser,
+  passport.authenticate('local', { failureRedirect: '/' }),
+  (req, res) => {
+    console.log('Registration successful for \x1b[33m%s\x1b[0m', req.user.username);
+    res.redirect('/profile');
+  }
+);
+
+app.post('/login', passport.authenticate('local', { failureRedirect: '/' }), (req, res) => {
   console.log('Auth successful for \x1b[33m%s\x1b[0m', req.user.username);
   res.redirect('/profile');
 });
